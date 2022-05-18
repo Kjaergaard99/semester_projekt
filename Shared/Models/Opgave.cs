e6190731083086d27bb20ee79fd145eef30cb464
danieldawsonjensen <dani540c@gmail.com>
2022-05-18 14:08:31 +0200

    nye klasser

---
+++ Shared/Models/Opgave.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace semester_projekt.Shared.Models
+{
+	public class Opgave
+	{
+		public int OpgaveId { get; set; }
+		public string OpgaveBeskrivelse { get; set; }
+
+
+		public Opgave()
+		{
+		}
+	}
+}
+
---
+++ Shared/Models/Rolle.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace semester_projekt.Shared.Models
+{
+	public class Rolle
+	{
+		public int RolleId { get; set; }
+		public string RolleNavn { get; set; }
+
+
+		public Rolle()
+		{
+		}
+	}
+}
+

cc6646491ed04594b5513fdf9703d343f29df7c8
danieldawsonjensen <dani540c@gmail.com>
2022-05-18 22:33:41 +0200

    jeg giver op godnat

--- Server/Models/FestivalDBContext.cs
+++ Server/Models/FestivalDBContext.cs
@@ -1,21 +1,21 @@
 ﻿using System;
 using Dapper;
 using Npgsql;
+using semester_projekt.Shared.Models;
 
 namespace semester_projekt.Server.Models
 {
 	public class FestivalDBContext
 	{
-		private readonly NpgsqlConnection npgsqlConnection; // ?
-
+		private readonly NpgsqlConnection npgsqlConnection; // er dette rigtigt?
 
 
 		public FestivalDBContext()
 		{
 			var db =
 				"User ID=postgres;Password=qrm49zyp;Host=localhost;Port=5432;Database=2_semester_projekt;";
+			
 		}
-
 
 
 	}
